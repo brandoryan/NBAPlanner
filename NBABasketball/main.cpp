c267c8b3d3811d9648a594c959ae423d7668e78d
naomidonato <36245167+naomidonato@users.noreply.github.com>
2018-11-14 15:41:25 -0800

    Database can be accessed from user's Desktop - QStandardPaths:

--- NBABasketball/main.cpp
+++ NBABasketball/main.cpp
@@ -1,18 +1,39 @@
 #include "login.h"
 #include <QApplication>
 #include <QtSql>
+#include <QStandardPaths>
+#include <QFile>
+
+#define DB_SUBFOLDER "Project2"
+#define DB_FILENAME "nba.db"
 
 int main(int argc, char *argv[])
 {
     QSqlDatabase db = QSqlDatabase::addDatabase(("QSQLITE"));
     db.setDatabaseName(QString("/Users/brandonryan/Desktop/GitHub/Project2/NBABasketball/nba.db"));
 
-    // Checking if database successfully opened
-    if(db.open()) {
-        qDebug() << "Opened";
+    QDir databaseDir(QStandardPaths::writableLocation(QStandardPaths::DesktopLocation));
+
+
+    qDebug() << "Looking here for the database: " << databaseDir.absoluteFilePath(DB_FILENAME);
+
+
+    if(QFile::exists(databaseDir.absoluteFilePath(DB_FILENAME)))
+    {
+        db.setDatabaseName(databaseDir.absoluteFilePath(DB_FILENAME));
+        db.setConnectOptions(databaseDir.absoluteFilePath(DB_FILENAME));
+
+        if(!db.open())
+        {
+            qDebug() << "Error: Connection failed";
+        }
+        else
+        {
+            qDebug() << "Datbase connection successful";
+        }
     }
-    else {
-        qDebug() << "Error";
+    else{
+        qDebug() << "Database file not found";
     }
 
     QApplication a(argc, argv);
--- build-NBABasketball-Desktop_Qt_5_11_1_MinGW_32bit-Debug/ui_admin.h
+++ build-NBABasketball-Desktop_Qt_5_11_1_MinGW_32bit-Debug/ui_admin.h
@@ -12,18 +12,85 @@
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QDialog>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QTableWidget>
 
 QT_BEGIN_NAMESPACE
 
 class Ui_admin
 {
 public:
+    QLabel *label;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton_5;
+    QPushButton *pushButton_6;
+    QPushButton *pushButton_7;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label_4;
+    QTableWidget *tableWidget;
+    QLabel *label_5;
 
     void setupUi(QDialog *admin)
     {
         if (admin->objectName().isEmpty())
             admin->setObjectName(QStringLiteral("admin"));
         admin->resize(1020, 730);
+        label = new QLabel(admin);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(470, 10, 81, 16));
+        pushButton = new QPushButton(admin);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(360, 500, 311, 32));
+        pushButton->setAutoDefault(false);
+        pushButton_2 = new QPushButton(admin);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        pushButton_2->setGeometry(QRect(360, 560, 311, 32));
+        pushButton_2->setAutoDefault(false);
+        pushButton_3 = new QPushButton(admin);
+        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
+        pushButton_3->setGeometry(QRect(360, 580, 311, 31));
+        pushButton_3->setAutoDefault(false);
+        pushButton_4 = new QPushButton(admin);
+        pushButton_4->setObjectName(QStringLiteral("pushButton_4"));
+        pushButton_4->setGeometry(QRect(250, 620, 521, 32));
+        pushButton_4->setAutoDefault(false);
+        pushButton_5 = new QPushButton(admin);
+        pushButton_5->setObjectName(QStringLiteral("pushButton_5"));
+        pushButton_5->setGeometry(QRect(360, 520, 311, 32));
+        pushButton_5->setAutoDefault(false);
+        pushButton_6 = new QPushButton(admin);
+        pushButton_6->setObjectName(QStringLiteral("pushButton_6"));
+        pushButton_6->setGeometry(QRect(360, 540, 311, 32));
+        pushButton_6->setAutoDefault(false);
+        pushButton_7 = new QPushButton(admin);
+        pushButton_7->setObjectName(QStringLiteral("pushButton_7"));
+        pushButton_7->setGeometry(QRect(250, 600, 521, 32));
+        pushButton_7->setAutoDefault(false);
+        label_2 = new QLabel(admin);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(460, 480, 101, 16));
+        label_3 = new QLabel(admin);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setGeometry(QRect(70, 500, 171, 51));
+        label_3->setFrameShape(QFrame::Box);
+        label_3->setWordWrap(true);
+        label_4 = new QLabel(admin);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setGeometry(QRect(760, 500, 161, 91));
+        label_4->setFrameShape(QFrame::Box);
+        label_4->setWordWrap(true);
+        tableWidget = new QTableWidget(admin);
+        tableWidget->setObjectName(QStringLiteral("tableWidget"));
+        tableWidget->setGeometry(QRect(10, 30, 991, 431));
+        label_5 = new QLabel(admin);
+        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setGeometry(QRect(670, 545, 91, 16));
 
         retranslateUi(admin);
 
@@ -32,7 +99,19 @@ public:
 
     void retranslateUi(QDialog *admin)
     {
-        admin->setWindowTitle(QApplication::translate("admin", "Dialog", nullptr));
+        admin->setWindowTitle(QApplication::translate("admin", "Administrator Menu", nullptr));
+        label->setText(QApplication::translate("admin", "Data Viewer", nullptr));
+        pushButton->setText(QApplication::translate("admin", "All Teams By Team Name", nullptr));
+        pushButton_2->setText(QApplication::translate("admin", "All Teams By Arena Name", nullptr));
+        pushButton_3->setText(QApplication::translate("admin", "Eastern Conference Teams by Team Name", nullptr));
+        pushButton_4->setText(QApplication::translate("admin", "Southeast Division Teams of Eastern Conference by Team Name", nullptr));
+        pushButton_5->setText(QApplication::translate("admin", "Coaches and Team Names by Team name", nullptr));
+        pushButton_6->setText(QApplication::translate("admin", "NBA Arenas and Teams by Seating Capacity", nullptr));
+        pushButton_7->setText(QApplication::translate("admin", "NBA Teams, Arena Names, and Year Joined Sorted by Year in ascending order", nullptr));
+        label_2->setText(QApplication::translate("admin", "Display Options", nullptr));
+        label_3->setText(QApplication::translate("admin", "Double Click a Team to see all information related to it, including souvenirs", nullptr));
+        label_4->setText(QApplication::translate("admin", "As well as total capacity of all NBA teams, if two teams use the same arena, count the capacity of the arena only once", nullptr));
+        label_5->setText(QApplication::translate("admin", "<-------------", nullptr));
     } // retranslateUi
 
 };
